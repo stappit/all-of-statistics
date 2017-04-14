55e73d35852b7c14379250f7663be98f38816a9f
Brian <briancallander@gmail.com>
2017-04-14 18:40:40 +0200

    bootstrap statistic can now take arbitrary arguments

--- chapters/chapter8.py
+++ chapters/chapter8.py
@@ -4,14 +4,25 @@ from .chapter7 import CI
 
 
 def bootstrap(observations, statistic, iters):
+    """
+    Yield the statistic applied to bootstapped samples.
+
+    :observations: DataFrame,
+        cols are random variables
+        rows are observations
+
+    :statistic: the statistic to be calculated on each sample
+    :iters: int, the number of iterations
+    """
+
     n = len(observations)
     for _ in range(iters):
-        yield statistic(np.random.choice(observations, n))
+        yield statistic(observations.sample(n, replace=True))
 
 
 def bootstrap_variance(observations, statistic, iters):
     boots = list(bootstrap(observations, statistic, iters))
-    vboot = np.var(boots)
+    vboot = np.var(boots, axis=0)
     return vboot
 
 
@@ -33,6 +44,6 @@ def bootstrap_ci_pivot(observations, statistic, iters, a):
 
 def bootstrap_ci_percentile(observations, statistic, iters, a):
     boots = list(bootstrap(observations, statistic, iters))
-    theta_l = np.percentile(boots, 100*a / 2)
-    theta_r = np.percentile(boots, 100 - 100*a / 2)
+    theta_l = np.percentile(boots, 100*a / 2, axis=0)
+    theta_r = np.percentile(boots, 100 - 100*a / 2, axis=0)
     return CI(theta_l, theta_r)
