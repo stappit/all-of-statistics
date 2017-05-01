c43c7af3f61bc1c4d5ff1db3117df6729bb202a4
Brian <briancallander@gmail.com>
2017-05-01 22:35:55 +0200

    added random state to bootstap functions

--- chapters/chapter8.py
+++ chapters/chapter8.py
@@ -3,7 +3,7 @@ from scipy.stats import norm
 from .chapter7 import CI
 
 
-def bootstrap(observations, statistic, iters):
+def bootstrap(observations, statistic, iters, random_state=0):
     """
     Yield the statistic applied to bootstapped samples.
 
@@ -16,34 +16,37 @@ def bootstrap(observations, statistic, iters):
     """
 
     n = len(observations)
-    for _ in range(iters):
-        yield statistic(observations.sample(n, replace=True))
+    for i in range(iters):
+        yield statistic(observations.sample(n, replace=True,
+                                            random_state=random_state+i))
 
 
-def bootstrap_variance(observations, statistic, iters):
-    boots = list(bootstrap(observations, statistic, iters))
+def bootstrap_variance(observations, statistic, iters, random_state=0):
+    boots = list(bootstrap(observations, statistic, iters,
+                           random_state=random_state))
     vboot = np.var(boots, axis=0)
     return vboot
 
 
-def bootstrap_ci_normal(observations, statistic, iters, a):
+def bootstrap_ci_normal(observations, statistic, iters, a, random_state=0):
     t = statistic(observations)
-    vboot = bootstrap_variance(observations, statistic, iters)
+    vboot = bootstrap_variance(observations, statistic, iters,
+                               random_state=random_state)
     seboot = np.sqrt(vboot)
     z = np.abs(norm.ppf(a / 2))
     return CI(t - z*seboot, t + z*seboot)
 
 
-def bootstrap_ci_pivot(observations, statistic, iters, a):
+def bootstrap_ci_pivot(observations, statistic, iters, a, random_state=0):
     theta = statistic(observations)
-    boots = list(bootstrap(observations, statistic, iters))
+    boots = list(bootstrap(observations, statistic, iters, random_state=random_state))
     theta_l = np.percentile(boots, 100 - 100*a / 2)
     theta_r = np.percentile(boots, 100*a / 2)
     return CI(2*theta - theta_l, 2*theta - theta_r)
 
 
-def bootstrap_ci_percentile(observations, statistic, iters, a):
-    boots = list(bootstrap(observations, statistic, iters))
+def bootstrap_ci_percentile(observations, statistic, iters, a, random_state=0):
+    boots = list(bootstrap(observations, statistic, iters, random_state=random_state))
     theta_l = np.percentile(boots, 100*a / 2, axis=0)
     theta_r = np.percentile(boots, 100 - 100*a / 2, axis=0)
     return CI(theta_l, theta_r)
